fa9df0ebdb2451858ec039d696c7950e62e83223
Janis <janismonique92@gmail.com>
2023-11-21 13:57:47 +0100

    unfinshed work

---
+++ gener.js
@@ -0,0 +1,18 @@
+const url = 'https://deezerdevs-deezer.p.rapidapi.com/genre/%7Bid%7D';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ffeee28345msh5c6941610732f11p142c84jsn084937dabdce',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
